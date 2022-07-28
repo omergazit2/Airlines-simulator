46b5eec38b365efc81406de441c0f99c5d3ae728
omergazit2 <omergazit2@gmail.com>
2022-07-28 12:50:00 +0300

    add config file

---
+++ create fligths/config/appsync_config.py
@@ -0,0 +1,7 @@
+# AppSync variables
+APPSYNC_API_ENDPOINT_URL = "<your endpoint>"
+appSyncUsername = "<your username>"
+appSyncPassword = "<your password>"
+appSyncClientID = "<your clientID>"
+appSyncUserPool = "<your userpool>"
+region = "<your region>"
